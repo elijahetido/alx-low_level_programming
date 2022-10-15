98111e1a5bbb5cd4e1a1dce6a7c47d3129ec3604
elijahetido <elijahetido@gmail.com>
2022-10-14 22:12:51 -0700

    C programming variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,12 +10,12 @@
 int main(void)
 {
 	int n, l;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	l = n % 10;
 	if (l > 5)
-       	{
+	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
 	else if (l == 0)
@@ -27,4 +27,4 @@ int main(void)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	}
 	return (0);
-}	
+}
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,16 +7,16 @@
 int main(void)
 {
 	int i;
-	
+
 	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 		if (i != 57)
 	{
 		putchar(',');
-		putchar(',');
+		putchar(' ');
 	}
 	}
 	putchar('\n');
-	return (0):
+	return (0);
 }
